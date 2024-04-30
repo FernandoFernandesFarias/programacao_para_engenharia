115962e9756b0642ea5702f408c1dc4c76623c17
FernandoFernandesFarias <fariasfernando028@gmail.com>
2024-04-29 23:20:02 -0300

    Fazendo alguns exercicios em python

---
+++ Meus exercicios/multiplicacao.py
@@ -0,0 +1,9 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+mult = 0
+
+for i in range(0, num1):
+  mult += num2
+
+print(mult)
---
+++ Meus exercicios/primeiros10quadrados.py
@@ -0,0 +1,7 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+quadrados = []
+
+for i in numeros:
+  quadrados.append(numeros[i - 1] ** 2)
+print(quadrados)
+
